32296e62f36d021e4996a3b5f491f6ffdc67ac9e
vladi <vladi03@hotmail.com>
2020-10-31 23:19:21 -0400

    add image scaling for uploading files

--- src/appRoutes.js
+++ src/appRoutes.js
@@ -2,16 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {multerUpload} = require("./file-middleware");
 const appRouter = express();
-appRouter.use(bodyParser.urlencoded({ extended: false }));
+appRouter.use(bodyParser.urlencoded({ extended: true }));
 appRouter.use(bodyParser.json());
 
 const {upload, storage } = multerUpload({bucketName : 'darbyBucket'});
+
 // It's very crucial that the file name matches the name attribute in your html
 appRouter.post('/', upload.single('myFile'),
     (req, res) => {
         //res.redirect('/');
         res.json(req.fileData);
 });
+//imageUpload
+appRouter.post('/image/:imageWidth', upload.single('myFile'),
+    (req, res) => {
+        res.json(req.fileData);
+    });
 
 appRouter.get('/:fileName', (req,res) => {
     storage.getFile(res, req.params.fileName);
